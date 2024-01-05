fbe09e778bdc80c295acc53b9fc25b63c3d4bb93
Kevin Fang <94582881+kfang1900@users.noreply.github.com>
2024-01-05 05:22:41 -0600

    fund page

---
+++ app/fund/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const FundPage = () => {
+  return (
+    <div>
+      {/* Content for the Donate page */}
+      <h1>Fund Page</h1>
+    </div>
+  )
+}
+
+export default FundPage
