233d9304cd6c00c332ba7764e33670bdff193be1
Kevin Fang <94582881+kfang1900@users.noreply.github.com>
2024-01-07 02:54:45 -0600

    Delete route.ts
    vercel test 3

--- app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log("Request received")
-  res.status(200).json({ message: "Hello" })
-}
